28123d009413e29228bef41ae33d87199e7b64c6
Olari Seppä <olari.seppa@tuni.fi>
2025-04-04 13:08:03 +0300

    Add password validation

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,12 +1,20 @@
 from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+
 from rest_framework import serializers
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["id", "username", "password"]
         extra_kwargs = {"password": {"write_only": True}}
-    
+
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
+        try:
+            validate_password(password=validated_data["password"], user=user)
+        except ValidationError as err:
+            raise serializers.ValidationError({"password": err.messages})
         return user
